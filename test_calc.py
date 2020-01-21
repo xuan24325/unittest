6b857df77f012c0b67e604b2cf969c6e8c38be91
Xuan l <xuan24325@gmail.com>
2020-01-21 09:02:48 -0800

    3rd ValueError

--- test_calc.py
+++ test_calc.py
@@ -14,14 +14,18 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.subtract(-1, 1),-2)
 
     def test_multiply(self):
-        self.assertEqual(calc.multiply(10, 5),50)
         self.assertEqual(calc.multiply(-1, -1),1)
         self.assertEqual(calc.multiply(-1, 1),-1)
+        self.assertEqual(calc.multiply(10, 5),50)
 
     def test_divide(self):
         self.assertEqual(calc.divide(10, 5),2)
         self.assertEqual(calc.divide(-1, -1),1)
         self.assertEqual(calc.divide(-1, 1),-1)
+
+        # self.assertRaises(ValueError, calc.divide, 10, 0)
+        with self.assertRaises(ValueError):
+            calc.divide(10,0)
 
 if __name__ == '__main__':
     unittest.main()
